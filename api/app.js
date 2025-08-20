565f6ddba0aacc9279b5305795f9c17f372ded0f
Hossein <hossein.aghili.2658@gmail.com>
2025-08-20 11:03:18 +0330

    Initial commit

---
+++ api/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import cors from "cors";
+import morgan from "morgan";
+
+const __filename =fileURLToPath(import.meta.url)
+export const __dirname = path.dirname(__filename)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+// app.use(express.static("Public"))
+
+
+
+export default app
---
+++ api/server.js
@@ -0,0 +1,14 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import { __dirname } from "./app.js";
+import mongoose from "mongoose";
+dotenv.configDotenv({ path: __dirname + "/config.env" })
+mongoose.connect(process.env.DATE_BASE).then(() => {
+    console.log("db is connected");
+}).catch((err) => {
+    console.log(err);
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running`);
+})
