09d9c91815e6de572902397764280b007dfc4131
Hossein <hossein.aghili.2658@gmail.com>
2025-09-02 23:03:47 +0330

    create functions controllers

---
+++ api/Controllers/address.controller.js
@@ -0,0 +1,19 @@
+import { catchAsync } from "vanta-api";
+import Address from "../Models/address.model.js";
+
+export const create = catchAsync(async(req,res,next)=>{
+
+})
+
+export const getAll = catchAsync(async(req,res,next)=>{
+
+})
+export const getOne = catchAsync(async(req,res,next)=>{
+
+})
+export const update = catchAsync(async(req,res,next)=>{
+
+})
+export const remove = catchAsync(async(req,res,next)=>{
+    
+})
