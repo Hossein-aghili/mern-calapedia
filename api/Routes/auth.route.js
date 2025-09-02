81a0ae28d37f7fd0b29bb77cc6c0db6de34e9fc5
Hossein <hossein.aghili.2658@gmail.com>
2025-09-02 23:23:33 +0330

    create routes auth

--- api/Routes/auth.route.js
+++ api/Routes/auth.route.js
@@ -1,3 +1,9 @@
 import express from 'express'
+import { auth, checkOtp, checkPassword, forgetPasseord, resendCode } from '../Controllers/auth.controller.js'
 const authRouter = express.Router()
+authRouter.route('/').post(auth)
+authRouter.route('/otp').post(checkOtp)
+authRouter.route('/password').post(checkPassword)
+authRouter.route('forget').post(forgetPasseord)
+authRouter.route('/resend').post(resendCode)
 export default authRouter
