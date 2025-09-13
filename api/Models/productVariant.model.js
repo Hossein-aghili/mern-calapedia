188f390e503fc68442fe2d29e6c90035c09149c8
Hossein <hossein.aghili.2658@gmail.com>
2025-09-13 09:54:39 +0330

    create base productVariant

---
+++ api/Models/productVariant.model.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+const productVariantSchema = new mongoose.Schema({
+
+})
+const ProductVariant = mongoose.model('productVariant',productVariantSchema)
+export default ProductVariant
