34d6252a5d6c46f7f52d4f01e87eccab865640fc
Hossein <hossein.aghili.2658@gmail.com>
2025-09-13 20:25:37 +0330

    create variant route

--- api/Routes/variant.route.js
+++ api/Routes/variant.route.js
@@ -1,3 +1,6 @@
 import express from 'express'
+import { create, getAll, getOne, remove, update } from '../Controllers/variant.controller.js'
 const variantRouter = express.Router()
+variantRouter.route('/').get(getAll).post(create)
+variantRouter.route('/:id').get(getOne).patch(update).delete(remove)
 export default variantRouter
