1cf8e96bebccd9e02c4f032287b501858d14e17e
Hossein <hossein.aghili.2658@gmail.com>
2025-09-13 20:23:48 +0330

    create route product

--- api/Routes/product.route.js
+++ api/Routes/product.route.js
@@ -1,3 +1,6 @@
 import express from 'express'
+import { create, getAll, getOne, remove, update } from '../Controllers/product.controller.js'
 const productRouter = express.Router()
+productRouter.route('/').get(getAll).post(create)
+productRouter.route('/:id').get(getOne).patch(update).delete(remove)
 export default productRouter
