f7bdb432860c750dca7ee006bec1bc238cbfb35d
Hossein <hossein.aghili.2658@gmail.com>
2025-09-13 11:19:51 +0330

    create model productSchema

--- api/Models/productVariant.model.js
+++ api/Models/productVariant.model.js
@@ -1,6 +1,32 @@
 import mongoose from "mongoose";
 const productVariantSchema = new mongoose.Schema({
-
-})
-const ProductVariant = mongoose.model('productVariant',productVariantSchema)
+    price: {
+        type: Number,
+        required: true,
+    },
+    discount: {
+        type: Number,
+        min: 0,
+        max: 100,
+        default: 0
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    variantId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Variant'
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+    priceAfterDiscount: {
+        type: number,
+        required: true
+    }
+}, { timestamps: true })
+const ProductVariant = mongoose.model('productVariant', productVariantSchema)
 export default ProductVariant
