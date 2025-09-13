fd44f4ea082b5cd97a9cee55a3829ca3348d327f
Hossein <hossein.aghili.2658@gmail.com>
2025-09-13 09:16:56 +0330

    create product model

--- api/Models/product.model.js
+++ api/Models/product.model.js
@@ -1,9 +1,38 @@
 import mongoose from "mongoose";
-const productShema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"وارد نام محصول خود را وارد اجباری است"]
-    }
+const informtionSchema = mongoose.Schema({
+
 })
-const Product = mongoose.model('Product',productShema)
+const productShema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "وارد نام محصول خود را وارد اجباری است"]
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'بارگذاری عکس اجباری است'],
+        default: []
+    },
+    informtion: {
+        type: [informtionSchema],
+        default: []
+    },
+    description: {
+        type: String,
+        required: [true, 'توضیحات اجباری است']
+    },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    brandId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand'
+    },
+    defaultProductVariant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'productVariant'
+    }
+
+}, { timestamps: true })
+const Product = mongoose.model('Product', productShema)
 export default Product;
