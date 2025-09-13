e8634af7fcc6fae5e1fe60bce0c15acbea33fe26
Hossein <hossein.aghili.2658@gmail.com>
2025-09-13 09:21:09 +0330

    create address route

--- api/Routes/address.route.js
+++ api/Routes/address.route.js
@@ -1,3 +1,6 @@
 import express from 'express'
+import { create,getAll,getOne,update,remove } from './../Controllers/address.controller.js';
 const addressRouter = express.Router()
+addressRouter.route('/').get(getAll).post(create)
+addressRouter.route('/:id').get(getOne).patch(update).delete(remove)
 export default addressRouter
