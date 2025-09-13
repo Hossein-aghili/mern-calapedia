7bf580279f6a65ac07f9175ecad6b25be48293e3
Hossein <hossein.aghili.2658@gmail.com>
2025-09-13 20:25:16 +0330

    create producVariant Route

--- api/Routes/productVariant.route.js
+++ api/Routes/productVariant.route.js
@@ -1,3 +1,6 @@
 import express from 'express'
+import { create, getAll, getOne, remove, update } from './../Controllers/productVariant.controller.js';
 const productVariantRouter = express.Router()
+productVariantRouter('/').get(getAll).post(create)
+productVariantRouter('/:id').get(getOne).patch(update).delete(remove)
 export default productVariantRouter
