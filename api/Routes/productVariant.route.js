ecd054ce9c9ae84dbbf47e0b0173aca735cedf75
Hossein <hossein.aghili.2658@gmail.com>
2025-09-14 21:08:31 +0330

    debog code productVariant

--- api/Routes/productVariant.route.js
+++ api/Routes/productVariant.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { create, getAll, getOne, remove, update } from './../Controllers/productVariant.controller.js';
 const productVariantRouter = express.Router()
-productVariantRouter('/').get(getAll).post(create)
-productVariantRouter('/:id').get(getOne).patch(update).delete(remove)
+productVariantRouter.route('/').get(getAll).post(create)
+productVariantRouter.route('/:id').get(getOne).patch(update).delete(remove)
 export default productVariantRouter
