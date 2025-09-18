794fa946bee5e6b84f16fcb45dd676160ceb3148
Hossein <hossein.aghili.2658@gmail.com>
2025-09-18 08:35:00 +0330

    create model carts

---
+++ api/Models/cart.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const itemSchema = mongoose.Schema({
+    productVariantId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ProductVariant'
+    },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+    quantity: {
+        type: Boolean,
+        default: 0
+    }
+})
+const cartSchema = mongoose.Schema({
+    items: {
+        type: [itemSchema],
+        default: []
+    },
+    totilPrice: {
+        type: Number,
+        default: 0
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+const Cart = mongoose.model('Cart', cartSchema)
+export default Cart
