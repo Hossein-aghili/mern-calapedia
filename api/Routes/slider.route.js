19b623ae380568ada35c50c227182f4c078bc9b5
Hossein <hossein.aghili.2658@gmail.com>
2025-09-20 10:16:51 +0330

    create route slider

--- api/Routes/slider.route.js
+++ api/Routes/slider.route.js
@@ -1,3 +1,7 @@
 import express from 'express'
+import { getAll, create, getOne, update, remove } from '../Controllers/slider.controller.js'
+import { isAdmin } from './../Middlewares/isAdmin.js';
 const sliderRouter = express.Router()
+sliderRouter.route('/').get(getAll).post(isAdmin, create)
+sliderRouter.route('/:id').get(isAdmin, getOne).patch(isAdmin, update).delete(isAdmin, remove)
 export default sliderRouter
