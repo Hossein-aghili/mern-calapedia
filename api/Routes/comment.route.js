616e29db0294d825d4800b6d803e554cdc92625f
Hossein <hossein.aghili.2658@gmail.com>
2025-09-20 10:16:28 +0330

    create route comments

--- api/Routes/comment.route.js
+++ api/Routes/comment.route.js
@@ -1,3 +1,9 @@
 import express from 'express'
+import { changeActivity, createComment, getAllComment, getOneComment, getProductComment, removeComment } from '../Controllers/comment.controller.js'
+import { isAdmin } from './../Middlewares/isAdmin.js';
+import { isLogin } from '../Middlewares/isLogin.js';
 const commentRouter = express.Router()
+commentRouter.route('/').get(isAdmin, getAllComment).post(isLogin, createComment)
+commentRouter.route('/post-comments/:id').get(getProductComment)
+commentRouter.route('/:id').get(isAdmin, getOneComment).patch(isAdmin, changeActivity).delete(isAdmin, removeComment)
 export default commentRouter
