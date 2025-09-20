80f53b84e9e3e9f673a38756342ab07bb0505029
Hossein <hossein.aghili.2658@gmail.com>
2025-09-20 10:16:05 +0330

    create route cart

--- api/Routes/cart.route.js
+++ api/Routes/cart.route.js
@@ -1,3 +1,5 @@
 import express from 'express'
+import { add, getUserCart, remove } from '../Controllers/cart.controller.js'
 const cartRouter = express.Router()
+cartRouter.route('/').post(add).get(getUserCart).patch(remove)
 export default cartRouter
