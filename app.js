cf6d5de6c4b827aab37d5fc49bbce8224dae8ed4
Hossein <hossein.aghili.2658@gmail.com>
2025-11-06 06:48:49 +0330

    Wirting middlewares

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import { fileURLToPath } from "url";
 import path from "path";
-
+import cors from 'cors'
+import morgan from 'morgan';
 
 const __filename =fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+export default app
